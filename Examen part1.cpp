00d4217f5a20e0f0f9be77a24ca5b0421f536b38
JonBT123 <106268017+JonBT123@users.noreply.github.com>
2022-06-16 12:38:07 -0500

    Add files via upload

---
+++ Examen part1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int numero; 
+
+int main(){
+
+div_t resultado;
+
+cout << "Ingrese un numero para verificar si es par" << endl;
+
+cin >> numero; // Con cout capturamos el numero ingresado.
+
+resultado=div(numero,2);
+
+if (resultado.rem>0)
+
+{
+
+cout << numero << "El numero es impar" << endl;
+
+}
+
+else {
+
+    cout << numero << "El numero es par" << endl;
+
+}
+
+
+
+cin.get();
+
+cin.ignore();
+
+return 0;
+
+}
